e8a2469f4a1092be1001df55976835861eb8c8b2
venkypapa <venkypapa6@gmail.com>
2022-03-26 21:41:32 +0530

    message

---
+++ src/Package/Class.java
@@ -0,0 +1,20 @@
+package Package;
+
+public class Class {
+
+	private void java() {
+		System.out.println("java tool");
+	}
+
+	private void selenium() {
+		System.out.println("selenium tool");
+	}
+
+	public static void main(String[] args) {
+		Class j = new Class();
+		j.java();
+		j.selenium();
+
+	}
+
+}
